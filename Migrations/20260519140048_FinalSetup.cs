6e21f93cf2daf94f8446671c7aa8e79c69cb170c
Marinel Cipu <marinel@Marinels-Mac-mini.local>
2026-05-19 17:01:49 +0300

    fixed an error when running the app due to db migration

---
+++ Migrations/20260519140048_FinalSetup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicfyWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class FinalSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
